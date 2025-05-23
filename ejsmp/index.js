6bf15e1cbf9398d6e053e7f017adc0c0c2fba308
codewithghouse <personalghouse@gmail.com>
2025-05-23 22:44:39 +0530

    hi

---
+++ ejsmp/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+let port = 3000;
+// serving the static files 
+app.use(express.static(path.join(__dirname,"/public")))
+//view engine ku set kare  to ejs
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.get("/",((req,res)=>{
+  console.log("renders")
+    res.render("index.ejs")
+
+}));
+
+app.get("/recipe/:recipe",((req,res)=>{
+    let { recipe}= req.params;
+    const recipedata= require("./recipe.json");
+     const data = recipedata[recipe];
+  if(data){
+     res.render("recipe.ejs",{ data});
+    console.log("the respponse send successfully")
+  }else{
+    res.render("error.ejs")
+  }
+}))
+// app.get("/ig/:username",((req,res)=>{
+//   let { username}= req.params;
+//   const instadata = require("./data.json");
+//   const data = instadata[username];
+//   if(data){
+//     res.render("insta.ejs",{ data});
+//   }else{
+//     res.render("error.ejs");
+//   }
+// }))
+
+
+app.listen(port,()=>{
+    console.log(`the app is listening in the port ${port}`);
+})
